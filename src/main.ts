a5ff505138a307c6c952210beb771d26c0c7a023
Jiang Sheng <jiangshengdev@outlook.com>
2024-05-22 16:15:37 +0800

    doc

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,76 @@
-// 定义 Option 类型
-type Option<T> = Some<T> | None;
+/**
+ * 表示一个可能存在或不存在的值的类型。
+ */
+type Maybe<T> = Some<T> | None;
 
-// Some 类表示存在的值
+/**
+ * 表示存在的值。
+ */
 class Some<T> {
   constructor(public value: T) {}
+
+  /**
+   * 检查是否为 Some。
+   * @returns 如果是 Some，返回 true；否则返回 false。
+   */
   isSome(): this is Some<T> {
     return true;
   }
+
+  /**
+   * 检查是否为 None。
+   * @returns 如果是 None，返回 true；否则返回 false。
+   */
   isNone(): this is None {
     return false;
   }
 }
 
-// None 类表示不存在的值
+/**
+ * 表示不存在的值。
+ */
 class None {
+  /**
+   * 检查是否为 Some。
+   * @returns 如果是 Some，返回 true；否则返回 false。
+   */
   isSome(): this is Some<never> {
     return false;
   }
+
+  /**
+   * 检查是否为 None。
+   * @returns 如果是 None，返回 true；否则返回 false。
+   */
   isNone(): this is None {
     return true;
   }
 }
 
-// 创建 Option 类型值的辅助函数
-function some<T>(value: T): Option<T> {
+/**
+ * 创建一个存在的值。
+ * @param value 值。
+ * @returns 一个包含该值的 Some 对象。
+ */
+function some<T>(value: T): Maybe<T> {
   return new Some(value);
 }
 
-function none(): Option<never> {
+/**
+ * 创建一个不存在的值。
+ * @returns 一个 None 对象。
+ */
+function none(): Maybe<never> {
   return new None();
 }
 
 // 使用示例
-function exampleFunction(value: number): Option<string> {
+/**
+ * 示例函数，根据输入值返回一个 Maybe 类型。
+ * @param value 输入值。
+ * @returns 如果输入值大于0，返回 Some；否则返回 None。
+ */
+function exampleFunction(value: number): Maybe<string> {
   if (value > 0) {
     return some('Value is positive');
   } else {
