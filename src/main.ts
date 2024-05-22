7f666abdb1712118549094c056d50d04aaca41f8
Jiang Sheng <jiangshengdev@outlook.com>
2024-05-22 09:53:29 +0800

    maybe

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,48 @@
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
+// 定义 Option 类型
+type Option<T> = Some<T> | None;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+// Some 类表示存在的值
+class Some<T> {
+  constructor(public value: T) {}
+  isSome(): this is Some<T> {
+    return true;
+  }
+  isNone(): this is None {
+    return false;
+  }
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+// None 类表示不存在的值
+class None {
+  isSome(): this is Some<never> {
+    return false;
+  }
+  isNone(): this is None {
+    return true;
+  }
+}
+
+// 创建 Option 类型值的辅助函数
+function some<T>(value: T): Option<T> {
+  return new Some(value);
+}
+
+function none(): Option<never> {
+  return new None();
+}
+
+// 使用示例
+function exampleFunction(value: number): Option<string> {
+  if (value > 0) {
+    return some('Value is positive');
+  } else {
+    return none();
+  }
+}
+
+const result = exampleFunction(1);
+if (result.isSome()) {
+  console.log(result.value); // 输出: Value is positive
+} else {
+  console.log('No value');
+}
